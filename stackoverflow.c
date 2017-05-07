a48468a3471f8cac2d76b76b028bab6d33331ba4
interfacekun <627795061@qq.com>
2017-05-07 14:51:50 +0800

    add comment 2017年 05月 07日 星期天 14:51:49 CST

--- stackoverflow.c
+++ stackoverflow.c
@@ -2,9 +2,13 @@
 #include <string.h>
 
 void overflow(char *arg){
+	//创建缓冲区256个字节
     char buf[256];
+    //打印缓冲区起始地址
     printf("ret_addr:%p\n", buf);
+    //将程序运行时入的参数复制到缓冲区中
     strcpy(buf, arg);
+    //打印缓冲区中的内容
     printf("%s\n", buf);
 }
 
