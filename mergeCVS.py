1b4981b0f0e048d1e592677eced427533ef8dc44
interfacekun <627795061@qq.com>
2018-01-10 21:17:33 +0800

    mergeCSV

--- mergeCVS.py
+++ mergeCVS.py
@@ -4,21 +4,24 @@
 import pandas as pd
 import os
 
-def mergeCVS:
+def mergeCSV():
+	print("Start merge this floder CSVs to mergeCSV.csv!")
 
 	folderPath = './'          
 	saveFilePath =  './'
-	saveFileName = 'all.csv'
+	saveFileName = 'mergeCSV.csv'
 
 	os.chdir(folderPath)
 	fileList = os.listdir()
 
-	print("")
-
 	fileObject = pd.read_csv(folderPath + fileList[0])
-
 	fileObject.to_csv(saveFilePath + saveFileName, encoding="utf_8_sig", index=False, header=False)
 
-	for i in range(1,len(fileLlist)):
+	for i in range(1,len(fileList)):
 		fileObject = pd.read_csv(folderPath + fileList[i])
 		fileObject.to_csv(saveFilePath + saveFileName, encoding="utf_8_sig", index=False, header=False, mode='a+')
+	
+	print("Merge CSVs done! Total merge" + len(fileList) + "files.")
+
+if __name__ == '__main__':
+	mergeCSV()
